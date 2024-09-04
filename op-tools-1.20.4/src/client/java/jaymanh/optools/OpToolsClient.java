f4bb841e13cc82f96450d467bcc9c30b913c64a2
jaymanh <65929600+jaymanh@users.noreply.github.com>
2024-09-04 13:02:35 +1000

    Template upload

---
+++ op-tools-1.20.4/src/client/java/jaymanh/optools/OpToolsClient.java
@@ -0,0 +1,10 @@
+package jaymanh.optools;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class OpToolsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
