f2e690818d20dbb99ed32cb85286cc1efdf3cbd5
jaymanh <65929600+jaymanh@users.noreply.github.com>
2024-09-12 12:16:03 +1000

    added texture

--- op-tools-1.20.4/src/main/java/jaymanh/optools/WorkStation/RefineryBlockEntity.java
+++ op-tools-1.20.4/src/main/java/jaymanh/optools/WorkStation/RefineryBlockEntity.java
@@ -5,6 +5,7 @@ import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
+import static jaymanh.optools.OpTools.LOGGER;
 
 public class RefineryBlockEntity extends BlockEntity {
 
@@ -13,6 +14,7 @@ public class RefineryBlockEntity extends BlockEntity {
     }
 
     public static void tick(World world, BlockPos pos, BlockState state, RefineryBlockEntity blockEntity){
+        //runs once per game tick
 
         return;
     }
