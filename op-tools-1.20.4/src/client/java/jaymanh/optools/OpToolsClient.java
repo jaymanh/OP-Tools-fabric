ba2b97d7380d8dee807d6ef9f8a9366a9cb9fc6b
jaymanh <65929600+jaymanh@users.noreply.github.com>
2024-09-17 09:54:12 +1000

    idk

--- op-tools-1.20.4/src/client/java/jaymanh/optools/OpToolsClient.java
+++ op-tools-1.20.4/src/client/java/jaymanh/optools/OpToolsClient.java
@@ -1,10 +1,13 @@
 package jaymanh.optools;
 
+import jaymanh.optools.GUI.Screen.ModScreenHandlers;
+import jaymanh.optools.GUI.Screen.RefineryScreen;
 import net.fabricmc.api.ClientModInitializer;
+import net.minecraft.client.gui.screen.ingame.HandledScreens;
 
 public class OpToolsClient implements ClientModInitializer {
 	@Override
-	public void onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	public void onInitializeClient(){
+		HandledScreens.register(ModScreenHandlers.REFINERY_SCREEN_HANDLER, RefineryScreen::new);
 	}
 }
