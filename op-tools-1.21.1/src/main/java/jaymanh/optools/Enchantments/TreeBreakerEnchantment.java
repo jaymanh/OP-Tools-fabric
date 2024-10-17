a507eba0ffb74cb7cc22170de7650899b4b56972
jaymanh <65929600+jaymanh@users.noreply.github.com>
2024-10-17 13:32:09 +1100

    Update TreeBreakerEnchantment.java

--- op-tools-1.21.1/src/main/java/jaymanh/optools/Enchantments/TreeBreakerEnchantment.java
+++ op-tools-1.21.1/src/main/java/jaymanh/optools/Enchantments/TreeBreakerEnchantment.java
@@ -7,25 +7,81 @@ import net.minecraft.enchantment.EnchantmentEffectContext;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 
 import static com.mojang.text2speech.Narrator.LOGGER;
 
 public class TreeBreakerEnchantment {
 
-    private static void BreakTrees(BlockPos blockPos, Block woodType, World world, int level, EnchantmentEffectContext context, Entity user){
-        BlockPos currentBlock = blockPos;
-        
+    private static final int MAX_BLOCKS = 1000;
+
+    private static void BreakTrees(BlockPos blockPos, Block woodType, World world, int level, EnchantmentEffectContext context, Entity user) {
+        ArrayList<BlockPos> blockPosList = new ArrayList<>();
+        blockPosList.add(blockPos);
+
+        while (!blockPosList.isEmpty()) {
+            ArrayList<BlockPos> newPositions = new ArrayList<>();
+            Set<BlockPos> seenPositions = new HashSet<>();
+
+            Iterator<BlockPos> iterator = blockPosList.iterator();
+
+            while (iterator.hasNext()) {
+                BlockPos currentPos = iterator.next();
+
+                world.breakBlock(currentPos, true);
+                iterator.remove();
+
+                for (Direction direction : Direction.values()) {
+                    BlockPos offsetPos = currentPos.offset(direction);
+
+                    if (world.getBlockState(offsetPos).getBlock() == woodType && seenPositions.add(offsetPos)) {
+                        newPositions.add(offsetPos);
+                    }
+                }
+
+                for (Direction direction1 : Direction.values()) {
+                    for (Direction direction2 : Direction.values()) {
+                        if (direction1 != direction2 && direction1.getAxis() != direction2.getAxis()) {
+                            BlockPos diagonalOffset = currentPos.offset(direction1).offset(direction2);
+                            if (world.getBlockState(diagonalOffset).getBlock() == woodType && seenPositions.add(diagonalOffset)) {
+                                newPositions.add(diagonalOffset);
+                            }
+                        }
+                    }
+                }
+
+                if (newPositions.size() > MAX_BLOCKS) {
+                    for (BlockPos pos : blockPosList) {
+                        world.breakBlock(pos, true);
+                    }
+                    return;
+                }
+            }
+
+            blockPosList.addAll(newPositions);
+        }
     }
+
+
+
+
+
 
     public static void initialise(){
         OpTools.register(Identifier.of("tree_breaker_hit"), (world, level, context, user, pos) -> {
             BlockPos blockPos = new BlockPos((int)pos.x, (int)pos.y,(int)pos.z);
             LOGGER.info(world.getBlockState(blockPos).toString());
 
-            if(world.getBlockState(blockPos).getBlock() == Blocks.OAK_LOG){
+            if(world.getBlockState(blockPos).getBlock() == Blocks.OAK_LOG || world.getBlockState(blockPos).getBlock() == Blocks.SPRUCE_LOG){
                 BreakTrees(blockPos, world.getBlockState(blockPos).getBlock(), world, level, context, user);
             }
+
         });
     }
 }
