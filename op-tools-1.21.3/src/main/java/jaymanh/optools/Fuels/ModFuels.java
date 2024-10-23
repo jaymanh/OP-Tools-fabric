24cd5f86fcb8dc2b0d5ed027e83a3df744b1b058
jaymanh <65929600+jaymanh@users.noreply.github.com>
2024-10-24 09:54:49 +1100

    Update ModFuels.java
    Fixed Fuel

--- op-tools-1.21.3/src/main/java/jaymanh/optools/Fuels/ModFuels.java
+++ op-tools-1.21.3/src/main/java/jaymanh/optools/Fuels/ModFuels.java
@@ -1,7 +1,7 @@
 package jaymanh.optools.Fuels;
 
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
-import net.fabricmc.fabric.api.registry.FuelRegistry;
+import net.fabricmc.fabric.api.registry.FuelRegistryEvents;
 import net.minecraft.item.Item;
 import net.minecraft.util.Rarity;
 
@@ -19,6 +19,9 @@ public class ModFuels {
         ItemGroupEvents.modifyEntriesEvent(OP_TOOLS_ITEM_GROUP_KEY).register(itemGroup -> {
             itemGroup.add(SUPER_FUEL);
         });
-        FuelRegistry.INSTANCE.add(SUPER_FUEL, 20 * 640);
+
+        FuelRegistryEvents.BUILD.register(((builder, context) -> {
+            builder.add(ModFuels.SUPER_FUEL, 20 * 640);
+        }));
     }
 }
