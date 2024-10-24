09b70bb528fdc8e61cb3f2d87f698fbe14fc2a85
jaymanh <65929600+jaymanh@users.noreply.github.com>
2024-10-24 14:08:30 +1100

    Update ModArmorMaterials.java

--- op-tools-1.21.3/src/main/java/jaymanh/optools/Armor/ModArmorMaterials.java
+++ op-tools-1.21.3/src/main/java/jaymanh/optools/Armor/ModArmorMaterials.java
@@ -1,8 +1,12 @@
 package jaymanh.optools.Armor;
 
 import jaymanh.optools.Items.ModItems;
+import net.fabricmc.fabric.api.item.v1.FabricItem;
 import net.minecraft.item.ArmorItem;
+import net.minecraft.item.Item;
 import net.minecraft.item.equipment.ArmorMaterial;
+import net.minecraft.item.equipment.EquipmentType;
+import net.minecraft.item.equipment.trim.ArmorTrim;
 import net.minecraft.recipe.Ingredient;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
@@ -18,8 +22,7 @@ import java.util.function.Supplier;
 import static jaymanh.optools.OpTools.MOD_ID;
 
 public class ModArmorMaterials {
-
-    public static RegistryEntry<ArmorMaterial> registerMaterial(String id, Map<ArmorItem.Type, Integer> defensePoints, int enchantability, RegistryEntry<SoundEvent> equipSound, Supplier<Ingredient> repairIngredientSupplier, float toughness, float knockbackResistance, boolean dyeable) {
+    public static RegistryEntry<ArmorMaterial> registerMaterial(String id, Map<EquipmentType, Integer> defensePoints, int enchantability, RegistryEntry<SoundEvent> equipSound, Supplier<Ingredient> repairIngredientSupplier, float toughness, float knockbackResistance, boolean dyeable) {
         List<ArmorMaterial.Layer> layers = List.of(
                 new ArmorMaterial.Layer(Identifier.of(MOD_ID, id), "", dyeable)
         );
@@ -33,10 +36,10 @@ public class ModArmorMaterials {
 
     public static final RegistryEntry<ArmorMaterial> DIAMONDILLIUM = registerMaterial("diamondillium",
             Map.of(
-                    ArmorItem.Type.HELMET, 5,
-                    ArmorItem.Type.CHESTPLATE, 9,
-                    ArmorItem.Type.LEGGINGS, 7,
-                    ArmorItem.Type.BOOTS, 4
+                    EquipmentType.HELMET, 5,
+                    EquipmentType.CHESTPLATE, 9,
+                    EquipmentType.LEGGINGS, 7,
+                    EquipmentType.BOOTS, 4
             ),
             24,
             SoundEvents.ITEM_ARMOR_EQUIP_NETHERITE,
@@ -47,10 +50,10 @@ public class ModArmorMaterials {
 
     public static final RegistryEntry<ArmorMaterial> DIAMONDIUM = registerMaterial("diamondium",
             Map.of(
-                    ArmorItem.Type.HELMET, 7,
-                    ArmorItem.Type.CHESTPLATE, 15,
-                    ArmorItem.Type.LEGGINGS, 11,
-                    ArmorItem.Type.BOOTS, 6
+                    EquipmentType.HELMET, 7,
+                    EquipmentType.CHESTPLATE, 15,
+                    EquipmentType.LEGGINGS, 11,
+                    EquipmentType.BOOTS, 6
             ),
             24,
             SoundEvents.ITEM_ARMOR_EQUIP_NETHERITE,
