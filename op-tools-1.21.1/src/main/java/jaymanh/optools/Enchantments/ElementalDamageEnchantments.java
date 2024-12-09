7d6c43fbbd17586f006c324e469155a513d0cddc
jaymanh <65929600+jaymanh@users.noreply.github.com>
2024-12-10 10:57:53 +1100

    Finished Fire enchantment

--- op-tools-1.21.1/src/main/java/jaymanh/optools/Enchantments/ElementalDamageEnchantments.java
+++ op-tools-1.21.1/src/main/java/jaymanh/optools/Enchantments/ElementalDamageEnchantments.java
@@ -4,21 +4,35 @@ import jaymanh.optools.OpTools;
 import net.minecraft.block.Blocks;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 import static jaymanh.optools.OpTools.LOGGER;
 
 public class ElementalDamageEnchantments {
 
+    private static void createFireRadius(BlockPos center, World world, int radius) {
+        for (int x = -radius; x <= radius; x++) {
+            for (int y = -radius; y <= radius; y++) {
+                for (int z = -radius; z <= radius; z++) {
+                    BlockPos posToCheck = center.add(x, y, z);
+                    if (x * x + y * y + z * z <= radius * radius) {
+                        if (world.isAir(posToCheck) || world.getBlockState(posToCheck).getBlock() == Blocks.AIR || world.getBlockState(posToCheck).getBlock() == Blocks.TALL_GRASS || world.getBlockState(posToCheck).getBlock() == Blocks.SHORT_GRASS) {
+                            world.setBlockState(posToCheck, Blocks.FIRE.getDefaultState());
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
+
     public static void initialise(){
         OpTools.register(Identifier.of("elemental_fire"), (world, level, context, user, pos) -> {
-            LOGGER.info("World: " + world.toString());
-            LOGGER.info("Level: " + level);
-            LOGGER.info("Context: " + context.toString());
-            LOGGER.info("User: " + user.toString());
-            LOGGER.info("Pos: " + pos.toString());
             BlockPos victimBlockPos = user.getBlockPos();
-            world.setBlockState(victimBlockPos, Blocks.FIRE.getDefaultState());
-            //make an effect that lasts for x seconds to prolong enchantment 
+            level--;
+            createFireRadius(victimBlockPos, world, level);
+            //world.setBlockState(victimBlockPos, Blocks.FIRE.getDefaultState());
         });
         OpTools.register(Identifier.of("elemental_water"), (world, level, context, user, pos) -> {
 
