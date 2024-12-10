25c3593e7cc6c748ae3caf10e957c59e5529d164
jaymanh <65929600+jaymanh@users.noreply.github.com>
2024-12-10 14:28:05 +1100

    Seems to be working correctly

--- op-tools-1.21.1/src/main/java/jaymanh/optools/Enchantments/EnchantTick.java
+++ op-tools-1.21.1/src/main/java/jaymanh/optools/Enchantments/EnchantTick.java
@@ -9,34 +9,42 @@ import net.minecraft.util.TypeFilter;
 
 import static jaymanh.optools.OpTools.LOGGER;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
 import java.util.Set;
 
-public class EnchantTick implements ServerTickEvents.EndWorldTick{
-    public static void initialise(){
+public class EnchantTick implements ServerTickEvents.EndWorldTick {
+    public static void initialise() {
         ServerTickEvents.END_WORLD_TICK.register(new EnchantTick());
     }
+
     @Override
     public void onEndTick(ServerWorld world) {
         List<? extends LivingEntity> entitiesList = world.getEntitiesByType(TypeFilter.instanceOf(LivingEntity.class), EntityPredicates.VALID_ENTITY);
-        for(Entity entity: entitiesList){
-            Set<String> tag = entity.getCommandTags();
-            for(String cTag : tag){
-                if(Objects.equals(cTag, "Gravity")){
-                    LOGGER.info("test");
-                    Set<String> tags = entity.getCommandTags();
-                    for(String cTags : tags){
-                        if(cTags.startsWith("Num")){
-                            int cur = (int)cTags.charAt(cTags.length() -1);
-                            if(cur == 100){
-                                entity.setNoGravity(false);
-                                entity.removeCommandTag("Num100");
-                                entity.removeCommandTag("Gravity");
+        for (Entity entity : entitiesList) {
+            Set<String> tags = new HashSet<>(entity.getCommandTags());
+            for (String cTag : tags) {
+                if (Objects.equals(cTag, "Gravity")) {
+                    for (String cTags : tags) {
+                        if (cTags.startsWith("Num")) {
+                            String numPart = cTags.substring(3);
+                            int currentNum;
+                            try {
+                                currentNum = Integer.parseInt(numPart);
+                            } catch (NumberFormatException e) {
+                                LOGGER.error("Invalid number format for tag: " + cTags);
+                                continue;
                             }
-                            else{
-                                cur++;
-                                entity.addCommandTag("Num" + cur);
+
+                            if (currentNum == 100) {
+                                entity.setNoGravity(false);
+                                entity.removeCommandTag("Num" + numPart);
+                                entity.removeCommandTag("Gravity");
+                            } else {
+                                entity.removeCommandTag("Num" + numPart);
+                                int nextNum = currentNum + 1;
+                                entity.addCommandTag("Num" + nextNum);
                             }
                         }
                     }
