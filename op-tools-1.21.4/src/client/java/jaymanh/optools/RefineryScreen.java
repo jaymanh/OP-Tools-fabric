b5a9dd9357652a8fa5bb9bf9d500fb5ce85ff73d
jaymanh <65929600+jaymanh@users.noreply.github.com>
2025-02-27 10:28:37 +1100

    Update RefineryScreen.java
    I think this should work now.

--- op-tools-1.21.4/src/client/java/jaymanh/optools/RefineryScreen.java
+++ op-tools-1.21.4/src/client/java/jaymanh/optools/RefineryScreen.java
@@ -1,10 +1,9 @@
 package jaymanh.optools;
 
-import com.mojang.blaze3d.systems.RenderSystem;
+import net.minecraft.client.render.RenderLayer;
 import jaymanh.optools.GUI.Screen.RefineryScreenHandler;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.ingame.HandledScreen;
-import net.minecraft.client.render.GameRenderer;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
@@ -27,20 +26,16 @@ public class RefineryScreen extends HandledScreen<RefineryScreenHandler> {
 
     @Override
     protected void drawBackground(DrawContext context, float delta, int mouseX, int mouseY) {
-        RenderSystem.setShader(GameRenderer::getPositionTexProgram);
-        RenderSystem.setShaderColor(1f, 1f, 1f, 1f);
-        RenderSystem.setShaderTexture(0, TEXTURE);
         int x = (width - backgroundWidth) / 2;
         int y = (height - backgroundHeight) / 2;
 
-        context.drawTexture(TEXTURE, x, y, 0, 0, backgroundWidth, backgroundHeight);
-
+        context.drawTexture(RenderLayer::getGuiTextured,TEXTURE, x, y, 0, 0, backgroundWidth, backgroundHeight,256,256);
         renderProgressArrow(context, x, y);
     }
 
     private void renderProgressArrow(DrawContext context, int x, int y) {
         if(handler.isCrafting()) {
-            context.drawTexture(TEXTURE, x + 85, y + 30, 176, 0, 8, handler.getScaledProgress());
+            context.drawTexture(RenderLayer::getGuiTextured, TEXTURE, x + 85, y + 30, 176, 0, 8, handler.getScaledProgress(),256,256);
         }
     }
 
